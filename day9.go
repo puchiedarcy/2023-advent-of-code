30c6b8197a1f09ca4a2a4d644cf39951c9c3edde
Zachary Layman <zplayman@gmail.com>
2023-12-10 14:40:49 -0600

    day 9 part 2

--- day9.go
+++ day9.go
@@ -21,11 +21,30 @@ func RunDay9Part1() {
 	for scanner.Scan() {
 		lineText := scanner.Text()
 		numbers := helpers.ParseInts(lineText)
-		nextNumber := CalculateNextNumber(numbers)
+		nextNumber := CalculateFirstNumber(numbers)
 		sumValues += nextNumber
 	}
 
 	fmt.Println(sumValues)
+}
+
+func CalculateFirstNumber(currentLine []int) int {
+	nextLine := []int{}
+	allZeros := true
+	for i := 1; i < len(currentLine); i++ {
+		nextNumber := currentLine[i] - currentLine[i-1]
+		nextLine = append(nextLine, nextNumber)
+		if nextNumber != 0 {
+			allZeros = false
+		}
+	}
+
+	if allZeros {
+		return currentLine[0]
+	}
+
+	nextFirstNumber := CalculateFirstNumber(nextLine)
+	return currentLine[0] - nextFirstNumber
 }
 
 func CalculateNextNumber(currentLine []int) int {
