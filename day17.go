5cb74bae6f07c0bac07186dda3561931d433de42
Zachary Layman <zplayman@gmail.com>
2023-12-20 07:33:11 -0600

    day 17 part 1 WIP. yikes.

--- day17.go
+++ day17.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 )
+
+type HeatNode struct {
+	heatLoss      int
+	up            *HeatNode
+	upHeatLoss    int
+	down          *HeatNode
+	downHeatLoss  int
+	left          *HeatNode
+	leftHeatLoss  int
+	right         *HeatNode
+	rightHeatLoss int
+}
+
+func NewHeatNode(heatLoss int) *HeatNode {
+	newHeatNode := HeatNode{heatLoss: heatLoss}
+	return &newHeatNode
+}
 
 func RunDay17() {
 	file, err := os.Open("./inputs/day17inputsmall.txt")
@@ -15,7 +33,6 @@ func RunDay17() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	heatToEndMap := make(map[string]int)
 	theMap := make(map[int]string)
 	lineNumber := 0
 	for scanner.Scan() {
@@ -23,8 +40,81 @@ func RunDay17() {
 		lineNumber++
 	}
 
-	FindShortestPath(&theMap, &heatToEndMap)
+	destinationNode := NewHeatNode(getHeatValue(len(theMap)-1, len(theMap)-1, &theMap))
 
+	currentNode := destinationNode
+	row := len(theMap) - 1
+	// set all left and right on row
+	for col := len(theMap[row]) - 2; col >= 0; col-- {
+		leftNode := NewHeatNode(getHeatValue(row, col, &theMap))
+		currentNode.left = leftNode
+		leftNode.right = currentNode
+		currentNode = leftNode
+	}
+
+	// set all up down on col
+	currentNode = destinationNode
+	rowNode := currentNode
+	for col := len(theMap) - 1; col >= 0; col-- {
+		currentNode = rowNode
+		for upRow := len(theMap) - 2; upRow >= 0; upRow-- {
+			upNode := NewHeatNode(getHeatValue(upRow, col, &theMap))
+			currentNode.up = upNode
+			upNode.down = currentNode
+			currentNode = upNode
+		}
+		rowNode = rowNode.left
+	}
+
+	// connect all rows left and right
+	bottomRowNodes := []*HeatNode{}
+	currentNode = destinationNode
+	for currentNode != nil {
+		bottomRowNodes = append(bottomRowNodes, currentNode)
+		currentNode = currentNode.left
+	}
+
+	currentNode = bottomRowNodes[0]
+	nextNode := bottomRowNodes[1]
+	for nextNode != nil {
+		rightNode := currentNode.up
+		leftNode := nextNode.up
+
+		for leftNode != nil {
+			rightNode.left = leftNode
+			leftNode.right = rightNode
+
+			rightNode = rightNode.up
+			leftNode = leftNode.up
+		}
+
+		toBeCurrentNode := nextNode
+		nextNode = nextNode.left
+		currentNode = toBeCurrentNode
+	}
+
+	currentNode = destinationNode
+	for currentNode != nil {
+		tmpNextCurrent := currentNode.left
+		if tmpNextCurrent == nil {
+			break
+		} else {
+			currentNode = tmpNextCurrent
+		}
+	}
+
+	for currentNode != nil {
+		tmpNextCurrent := currentNode.up
+		if tmpNextCurrent == nil {
+			break
+		} else {
+			currentNode = tmpNextCurrent
+		}
+	}
+
+	sourceNode := currentNode
+
+	fmt.Println(currentNode)
 }
 
 func getHeatHash(row int, col int) string {
